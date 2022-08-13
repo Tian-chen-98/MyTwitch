6a8a18edd637618bf2800de9b9324364b2592bff
Tian Chen <tc3147@columbia.edu>
2022-08-13 19:38:03 -0400

    Initialize repo. Current files: - controller: GameController, SearchController - entity: db, response   - db: Item, ItemType   - response: Game - service: GameService, TwitchException - ApplicationConfig

---
+++ src/main/java/com/laioffer/jupiter/controller/GameController.java
@@ -0,0 +1,43 @@
+package com.laioffer.jupiter.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.service.GameService;
+import com.laioffer.jupiter.service.TwitchException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class GameController {
+  // this is field injection
+  @Autowired
+  private GameService gameService;
+  // this is constructor injection
+//   private final GameService gameService;
+//  @Autowired
+//  public GameController(GameService gameService) {
+//    this.gameService = gameService;
+//  }
+  
+  @RequestMapping(value = "/game", method = RequestMethod.GET)
+  public void getGame(@RequestParam(value = "game_name", required = false) String gameName, HttpServletResponse response) throws IOException, ServletException {
+    response.setContentType("application/json;charset=UTF-8");
+    try {
+      // Return the dedicated game information if gameName is provided in the request URL, otherwise return the top x games.
+      if (gameName != null) {
+        response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.searchGame(gameName)));
+      } else {
+        response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.topGames(0)));
+      }
+    } catch (TwitchException e) {
+      throw new ServletException(e);
+    }
+  }
+  
+}
